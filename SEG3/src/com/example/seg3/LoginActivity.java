bcd00f1861bb338737a32791520d31779da172eb
Julian Parker <julian.parker@kcl.ac.uk>
2014-02-11 22:11:19 +0000

    Update login activity (added objects)

--- SEG3/src/com/example/seg3/LoginActivity.java
+++ SEG3/src/com/example/seg3/LoginActivity.java
@@ -3,14 +3,29 @@ package com.example.seg3;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class LoginActivity extends Activity {
 
+	private String patientName;
+	TextView tvPatientName;
+	TimePicker appointmentTime;
+	Button loginButton;
+	 
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        tvPatientName = (TextView) findViewById(R.id.patientLoginName);
+        appointmentTime = (TimePicker) findViewById(R.id.patientAppointmentTime);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        patientName = (String) tvPatientName.getText();
     }
+    
 
 
     @Override
@@ -18,6 +33,10 @@ public class LoginActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.login, menu);
         return true;
+    }
+    
+    public String getPatientName() {
+    	return patientName;
     }
     
 }
