a6847b2792a9d9381b9db77eea204171365aef5f
Julian Parker <julian.parker@kcl.ac.uk>
2014-02-12 01:11:49 +0000

    Login button, onClick listener.

--- SEG3/src/com/example/seg3/LoginActivity.java
+++ SEG3/src/com/example/seg3/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.seg3;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -10,6 +13,8 @@ import android.widget.TimePicker;
 public class LoginActivity extends Activity {
 
 	private String patientName;
+	private int appointmentHour;
+	private int appointmentMinute;
 	TextView tvPatientName;
 	TimePicker appointmentTime;
 	Button loginButton;
@@ -20,10 +25,25 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         
+        //set TextView, TimePicker and Button
+        
         tvPatientName = (TextView) findViewById(R.id.patientLoginName);
         appointmentTime = (TimePicker) findViewById(R.id.patientAppointmentTime);
         loginButton = (Button) findViewById(R.id.loginButton);
-        patientName = (String) tvPatientName.getText();
+        loginButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View loginClick) {
+				// Store data re. name, time.  Direct user to video activity.
+				patientName = tvPatientName.getText().toString();
+				appointmentHour = appointmentTime.getCurrentHour();
+				
+				Log.v("hour", Integer.toString(appointmentHour));
+				Log.i("textview content", patientName);
+				
+			}
+		});
+        
     }
     
 
@@ -35,8 +55,13 @@ public class LoginActivity extends Activity {
         return true;
     }
     
+    public void setPatientName(String patientName) {
+		this.patientName = patientName;
+	}
+    
     public String getPatientName() {
-    	return patientName;
-    }
+		return patientName;
+	}
+    
     
 }
